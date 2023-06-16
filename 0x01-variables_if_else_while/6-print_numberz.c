cfd08a95f103b46da35ede5a146ad6851bf4d730
Kingsley Opoku <kobbykingsley@gmail.com>
2023-06-16 07:53:35 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include  <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always its 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar(a + 48);
+	}
+	putchar("\n")
+	return (0);
+}
