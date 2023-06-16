e6205b1b7cab1f824d6d6b234bd98439fb8c532d
Kingsley Opoku <kobbykingsley@gmail.com>
2023-06-16 07:41:27 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always its 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
